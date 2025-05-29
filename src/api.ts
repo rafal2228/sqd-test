f1eb31bd144b18636d2bda6051d9d30bd0e82eb0
rafal2228 <rafal.majchrzak@p2p.org>
2025-05-29 17:39:17 +0200

    Remove unused plugin

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,4 @@
 import SimplifyInflectorPlugin from "@graphile-contrib/pg-simplify-inflector";
-import AggregatesPluggin from "@graphile/pg-aggregates";
 import express from "express";
 import { NodePlugin } from "graphile-build";
 import { postgraphile } from "postgraphile";
@@ -15,7 +14,7 @@ const env = z
     DB_NAME: z.string().optional().default("squid"),
     DB_USER: z.string().optional().default("postgres"),
     DB_PASS: z.string().optional().default("postgres"),
-    BASE_PATH: z.string().optional(),
+    GRAPHQL_SERVER_BASE_PATH: z.string().optional(),
     GRAPHQL_SERVER_PORT: z.coerce.number().int().optional().default(8080),
   })
   .parse(process.env);
@@ -33,14 +32,16 @@ app.use(
     {
       graphiql: true,
       enhanceGraphiql: true,
-      dynamicJson: true,
+      dynamicJson: false,
       disableDefaultMutations: true,
-      disableQueryLog: true, // set to false to see the processed queries
+      disableQueryLog: true,
       skipPlugins: [NodePlugin],
-      appendPlugins: [AggregatesPluggin, FilterPlugin, SimplifyInflectorPlugin],
-      externalGraphqlRoute: env.BASE_PATH ? env.BASE_PATH + "/api/graphql" : undefined,
+      appendPlugins: [FilterPlugin, SimplifyInflectorPlugin],
+      externalGraphqlRoute: env.GRAPHQL_SERVER_BASE_PATH ? env.GRAPHQL_SERVER_BASE_PATH + "/api/graphql" : undefined,
       graphileBuildOptions: {
-        stateSchemas: ["evm"],
+        connectionFilterAllowedFieldTypes: ["String"],
+        connectionFilterComputedColumns: false,
+        connectionFilterAllowedOperators: ["inInsensitive"],
       },
     },
   ),
